a599430ed9fc8c21023c1aa7c0a260411cac4cee
chenglong.xu <1114027313@qq.com>
2020-05-28 22:24:18 +0800

    394

--- solutation/src/LeetCode394.java
+++ solutation/src/LeetCode394.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class LeetCode394 {
 
@@ -10,30 +11,37 @@ public class LeetCode394 {
 
     static class Solution {
         public String decodeString(String s) {
-            if (s == null || s.length() == 0) {
-                return s;
-            } else {
-                StringBuilder sb = new StringBuilder();
-                while (s.length() > 0) {
-                    int left = s.indexOf("[");
-                    int num = Integer.parseInt(s.substring(0, left));
-
-
-                    int right = s.indexOf("]");
-
-                    String minS = s.substring(left + 1, right);
-                    while (num > 0) {
-                        sb.append(minS);
-                        num--;
+            Stack<String> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == ']') {
+                    String string = "";
+                    while (!stack.peek().equals("[")) {
+                        string = stack.pop() + string;
                     }
-                    if (s.length() > right + 1) {
-                        s = s.substring(right + 1);
-                    } else {
-                        s = "";
+                    stack.pop();
+
+                    String countString = "";
+                    while ((!stack.isEmpty()) && (stack.peek().charAt(0) >= '0' && stack.peek().charAt(0) <= '9')) {
+                        countString = stack.pop() + countString;
                     }
+                    int count = Integer.parseInt(countString);
+
+                    String retString = "";
+                    for (int j = 0; j < count; j++) {
+                        retString = retString + string;
+                    }
+                    stack.push(retString);
+                } else {
+                    String str = "" + s.charAt(i);
+                    stack.push(str);
                 }
-                return sb.toString();
             }
+
+            String aaa = "";
+            while (!stack.isEmpty()) {
+                aaa = stack.pop() + aaa;
+            }
+            return aaa;
         }
     }
 
