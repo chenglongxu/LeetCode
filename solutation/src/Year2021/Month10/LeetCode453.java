2d65d24acc4a4cfd0c086fea35335ce72c40acfa
xuchenglong <1114027313@qq.com>
2021-10-20 22:11:43 +0800

    453

---
+++ solutation/src/Year2021/Month10/LeetCode453.java
@@ -0,0 +1,29 @@
+package Year2021.Month10;
+
+import java.util.Locale;
+
+public class LeetCode453 {
+
+    public static void main(String[] args) {
+        System.out.println(minMoves(new int[]{1, 2, 3}));
+        System.out.println(minMoves(new int[]{1, 1, 1}));
+    }
+
+    public static int minMoves(int[] nums) {
+        int min = 0;
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                min = nums[i];
+            }
+            if (min > nums[i]) {
+                total += (min - nums[i]) * i;
+                min = nums[i];
+            } else {
+                total += nums[i] - min;
+            }
+        }
+        return total;
+    }
+
+}
