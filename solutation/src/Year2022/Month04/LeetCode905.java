e7a704f466c63c3fd87e7a85c07ca1b8b30d6980
xuchenglong <xuchenglong@111.com.cn>
2022-04-29 10:49:29 +0800

    905

--- solutation/src/Year2022/Month04/LeetCode905.java
+++ solutation/src/Year2022/Month04/LeetCode905.java
@@ -10,15 +10,15 @@ public class LeetCode905 {
     public static int[] sortArrayByParity(int[] nums) {
         int[] newNums = new int[nums.length];
         int cursor = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                newNums[cursor] = nums[i];
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                newNums[cursor] = num;
                 cursor++;
             }
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 1) {
-                newNums[cursor] = nums[i];
+        for (int num : nums) {
+            if (num % 2 == 1) {
+                newNums[cursor] = num;
                 cursor++;
             }
         }
