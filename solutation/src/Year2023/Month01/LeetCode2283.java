0e6c52d49d9b12dc2da1a7b84ac0fde8ca0605c4
xuchenglong <xuchenglong@111.com.cn>
2023-01-11 21:23:46 +0800

    2283

---
+++ solutation/src/Year2023/Month01/LeetCode2283.java
@@ -0,0 +1,24 @@
+package Year2023.Month01;
+
+public class LeetCode2283 {
+
+    public static void main(String[] args) {
+        System.out.println(digitCount("1210"));
+        System.out.println(digitCount("030"));
+    }
+
+    public static boolean digitCount(String num) {
+        int[] nums = new int[10];
+        char[] ss = num.toCharArray();
+        for (char c : ss) {
+            nums[c - '0'] = nums[c - '0'] + 1;
+        }
+        for (int i = 0; i < ss.length; i++) {
+            if (nums[i] != ss[i] - '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
