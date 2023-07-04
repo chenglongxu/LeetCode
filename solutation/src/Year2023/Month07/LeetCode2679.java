4b781ea9e092a6bd9a9c9d5a0988f351d235e777
xuchenglong <xuchenglong@111.com.cn>
2023-07-04 21:23:31 +0800

    2679

---
+++ solutation/src/Year2023/Month07/LeetCode2679.java
@@ -0,0 +1,31 @@
+package Year2023.Month07;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.cn/problems/sum-in-a-matrix/
+ *
+ * @author xuchenglong
+ */
+public class LeetCode2679 {
+
+    public static void main(String[] args) {
+        System.out.println(matrixSum(new int[][]{new int[]{7, 2, 1}, new int[]{6, 4, 2}, new int[]{6, 5, 3}, new int[]{3, 2, 1}}));
+    }
+
+    public static int matrixSum(int[][] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Arrays.stream(nums[i]).sorted().toArray();
+        }
+        int returnValue = 0;
+        for (int i = 0; i < nums[0].length; i++) {
+            int max = 0;
+            for (int[] num : nums) {
+                max = Integer.max(num[i], max);
+            }
+            returnValue += max;
+        }
+        return returnValue;
+    }
+
+}
